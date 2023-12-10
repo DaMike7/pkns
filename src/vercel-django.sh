98c0ab27ef5971f29a2e208b9fec4023b2e59a35
DaMike7 <107225135+DaMike7@users.noreply.github.com>
2023-12-10 16:57:00 +0100

    Updated vercel-django.sh

--- src/vercel-django.sh
+++ src/vercel-django.sh
@@ -1,6 +1,6 @@
-. bin/activate
 pip install -r requirements.txt
 
 # Run migrations
 python3.9 manage.py migrate
 python3.9 manage.py runserver
+gunicorn pkns.wsgi:application -w 4 -k uvicorn.workers.UvicornWorker
