ea8fc2db846cf89a020d95789b2c87ac22d85b41
DaMike7 <michaeldabere462@gmail.com>
2024-05-16 00:17:13 +0100

    updated views.py

--- src/base/views.py
+++ src/base/views.py
@@ -1,11 +1,1 @@
-from django.shortcuts import render 
 from .models import Payment,Kid,Term
-from django.contrib.auth.decorators import login_required as lr
-
-@lr(login_url='login_page')
-def index(request): 
-	newest_payments = Payment.objects.all()[0:5]
-	newest_daycare_kids = Kid.objects.filter(kid_class="Daycare")[0:4]
-	newest_nursery_kids = Kid.objects.filter(kid_class="Nursery")[0:4]
-
-	return render(request, 'base/site/index.html',{'payments':newest_payments,'daycare_kids':newest_daycare_kids,'nursery_kids': newest_nursery_kids})
